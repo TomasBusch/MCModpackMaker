dca02612571a4244093fbb107e596a603ee2dc25
Tomibusch1 <tomibusch5@gmail.com>
2023-10-08 17:20:13 -0300

    Update eslint config

--- modpackmaker_react_client/.eslintrc.cjs
+++ modpackmaker_react_client/.eslintrc.cjs
@@ -10,6 +10,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    "semi": [2, "always"],
+    "no-extra-semi": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
