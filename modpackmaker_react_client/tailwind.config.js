461e77a0f88e89435bdb0040d58dd957bbef09af
Tomibusch1 <tomibusch5@gmail.com>
2023-10-08 17:22:05 -0300

    Add tailwind starting config

--- modpackmaker_react_client/tailwind.config.js
+++ modpackmaker_react_client/tailwind.config.js
@@ -1,17 +1,36 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
-const {nextui} = require("@nextui-org/react");
 
 export default {
+  plugins: [
+    // ...
+    require('tailwind-scrollbar')({ nocompatible: true }),
+  ],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-    darkMode: "class",
-  plugins: [nextui()],
-}
-
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  }
+};
