158b1d94b6cea45480f416fe257528aa65917cd0
Tomibusch1 <tomibusch5@gmail.com>
2023-10-09 18:55:55 -0300

    minor Refactor of project structure

--- modpackmaker_react_client/src/app.tsx
+++ modpackmaker_react_client/src/app.tsx
@@ -1,19 +1,28 @@
 // import Navbar from '../navbar/navbar';
-import { Link, Route, Routes } from "react-router-dom";
-import DefaultLayout from "../layouts/default/defaultLayout";
-import Search from "../screens/search/search";
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
-import Dashboard from "../screens/dashboard/dashboard";
-// import styles from './app.module.scss';
+import { Link, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./routes/layouts/default/defaultLayout";
+import Dashboard from "./routes/screens/dashboard/dashboard";
+import Search from "./routes/screens/search/search";
+// import backgroundImage from "./assets/images/background/pexels-johannes-plenio-1103970.jpg";
+import FullPageLayout from './routes/layouts/fullpage/fullpageLayout';
+import Login from './routes/screens/login/login';
+import Register from './routes/screens/register/register';
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <div className="h-screen bg-zinc-900 text-zinc-300">
+    <div className="h-screen bg-zinc-900 text-zinc-300 antialiased" 
+    // style={{
+    //   backgroundImage: `url(${backgroundImage})`,
+    //   backgroundSize: 'cover',
+      
+    // }}
+    >
           {/* Routes nest inside one another. Nested route paths build upon
                 parent route paths, and nested route elements render inside
               parent route elements. See the note about <Outlet> below. */}
@@ -28,7 +37,11 @@ export default function App() {
                     acts like a catch-all for URLs that we don't have explicit
                   routes for. */}
             </Route>
+            <Route element={<FullPageLayout />}>
+              <Route path='/login' element={<Login />}/>
+              <Route path='/register' element={<Register />}/>
               <Route path="*" element={<NoMatch />} />
+            </Route>
           </Routes>
           </QueryClientProvider>
         </div>
--- modpackmaker_react_client/src/main.tsx
+++ modpackmaker_react_client/src/main.tsx
@@ -3,8 +3,8 @@ import * as ReactDOM from "react-dom/client";
 import {
   BrowserRouter,
 } from "react-router-dom";
+import App from "./app";
 import "./index.scss";
-import App from "./routes/app/app";
 
 
 const rootElement = document.getElementById('root')!;
