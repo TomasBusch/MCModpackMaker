606a37e1428128cc30a32252c9d10755f3e1ff09
Tomibusch1 <tomibusch5@gmail.com>
2023-10-09 18:56:41 -0300

    Fix missing padding in small screens for search

--- modpackmaker_react_client/src/routes/layouts/default/defaultLayout.tsx
+++ modpackmaker_react_client/src/routes/layouts/default/defaultLayout.tsx
@@ -6,7 +6,7 @@ export default function DefaultLayout() {
       <>
         <div className="flex flex-col h-full gap-4">
           <Navbar /> 
-        <div className="flex-auto mt-0 overflow-y-auto md:mt-2">
+        <div className="flex-auto px-2 mt-0 overflow-y-auto md:px-0 md:mt-2">
           <Outlet />
         </div>
         </div>
