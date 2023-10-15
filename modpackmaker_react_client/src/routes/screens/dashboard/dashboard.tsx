736394ef7dba6bae3dbf868c83fe803b7f132712
Tomibusch1 <tomibusch5@gmail.com>
2023-10-15 15:19:48 -0300

    Add initial dashboard layout

--- modpackmaker_react_client/src/routes/screens/dashboard/dashboard.tsx
+++ modpackmaker_react_client/src/routes/screens/dashboard/dashboard.tsx
@@ -1,6 +1,12 @@
+import ModpackList from './modpackList';
 
 export default function Dashboard() {
   return (
-    <div>dashboard</div>
+    <>
+    <div className="flex flex-col items-center justify-items-center">
+      <h1 className='mt-4 text-2xl font-bold'>Modpacks</h1>
+      <ModpackList modpacks={["mod1", "mod2", "mod3", "mod4", "mod5", "mod6"]}/>
+    </div>
+    </>
   );
 }
